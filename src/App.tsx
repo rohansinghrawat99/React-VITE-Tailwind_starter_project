611a72ad925e49fc8a7ff274669574acc83957ba
Rohan Singh Rawat <rsrofficial99@gmail.com>
2021-09-29 17:12:36 +0530

    Basic Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+      <div className="flex flex-col justify-center items-center h-screen bg-black">
+        <h1 className="text-white">HELLO, I'M A REACT DEVELOPER!</h1>
+        <p className="text-white">- Rohan Singh Rawat</p>
+      </div>
+  )
+}
+
+export default App;
